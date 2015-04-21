5b010f95c3af43e6f8cd722c9bb83763ee07ed3f
Jose Aleman <jose.aleman@prodigious.com>
2015-04-21 10:06:18 -0600

    initial commit

---
+++ src/main/java/com/liftsimulation/controller/ControlPanelController.java
@@ -0,0 +1,13 @@
+package com.liftsimulation.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControlPanelController {
+
+	public ControlPanelController() {
+		
+	}
+
+	
+}
